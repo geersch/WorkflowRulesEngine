9cc53028d4920ac4e5ec6b30fb9476101e785a7c
Christophe Geers <geersch@gmail.com>
2014-05-31 16:39:48 +0200

    Added source code

---
+++ src/SharedClassLibrary/Person.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharedClassLibrary
+{
+    /// <summary>
+    /// A class that defines a person
+    /// </summary>
+    public class Person
+    {
+        #region Private instance variables
+
+        private String name;
+        private String firstname;
+        private DateTime birthDate;
+        private String gender;
+        private Boolean married;
+        private String salutation;
+
+        #endregion
+
+        #region Private methods
+
+        /// <summary>
+        /// Calculates the age of the person.
+        /// </summary>
+        /// <returns></returns>
+        private Int32 CalculateAge()
+        {
+            //Store today's date
+            DateTime now = DateTime.Today;
+            //Calculate the difference in years
+            Int32 years = now.Year - birthDate.Year;
+            //Subtract one year, if the current date is 
+            //before the birthday
+            if ((now.Month < birthDate.Month) || 
+                (now.Month == birthDate.Month && 
+                now.Day < birthDate.Day))
+            {
+                years--;
+            }
+            return years;
+        }
+
+        #endregion
+
+        #region Public properties
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public String Firstname
+        {
+            get { return firstname; }
+            set { firstname = value; }
+        }
+
+        public DateTime BirthDate
+        {
+            get { return birthDate; }
+            set { birthDate = value; }
+        }
+
+        public String Gender
+        {
+            get { return gender; }
+            set { gender = value; }
+        }
+
+        public Boolean Married
+        {
+            get { return married; }
+            set { married = value; }
+        }
+
+        /// <summary>
+        /// Readonly property which returns the
+        /// age of the person
+        /// </summary>
+        public Int32 Age
+        {
+            get { return CalculateAge(); }
+        }
+
+        /// <summary>
+        /// Readonly property that returns true
+        /// if the person is a minor
+        /// </summary>
+        public Boolean Minor
+        {
+            get { return (Age < 18); }
+        }
+
+        /// <summary>
+        /// Readonly property that returns true
+        /// if the person is an adult
+        /// </summary>
+        public Boolean Adult
+        {
+            get { return (Age >= 18); }
+        }
+
+        public String Salutation
+        {
+            get { return salutation; }
+            set { salutation = value; }
+        }
+
+        #endregion
+    }
+}
---
+++ src/SharedClassLibrary/RuleSetManager.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.IO;
+using System.Xml;
+using System.Workflow.ComponentModel.Compiler;
+using System.Workflow.ComponentModel.Serialization;
+using System.Workflow.Activities.Rules;
+
+namespace SharedClassLibrary
+{
+    public static class RuleSetManager
+    {
+        /// <summary>
+        /// Load a .rules resource (= manifestResource) from an assembly and
+        /// apply the specified RuleSet against an instance of a specified type
+        /// of object.
+        /// </summary>
+        /// <param name="assembly"></param>
+        /// <param name="manifestResource"></param>
+        /// <param name="ruleSet"></param>
+        /// <param name="objectType"></param>
+        /// <param name="instance"></param>
+        public static void ApplyRuleSet(Type assembly, String manifestResource,
+            String ruleSet, Type objectType, Object instance)
+        {
+            //Load the embedded .rules resource (= manifestResource parameter)
+            //from the specified assembly (= assembly parameter).            
+            Assembly assemblyResource = Assembly.GetAssembly(assembly);
+            Stream stream = assemblyResource.GetManifestResourceStream(manifestResource);
+
+            //The RuleSet is serialized and saved in Xml format.
+            using (XmlReader xmlReader = XmlReader.Create(new StreamReader(stream)))
+            {
+                //Deserialize the Xml string. The result is a RuleDefinitions object.
+                //The RuleDefinitions class is a container for the .rules file that 
+                //was just loaded. A .rules file can contain one or more RuleSets.
+                WorkflowMarkupSerializer markupSerializer =
+                    new WorkflowMarkupSerializer();
+                RuleDefinitions ruleDefinitions =
+                    markupSerializer.Deserialize(xmlReader) as RuleDefinitions;
+
+                if (ruleDefinitions != null)
+                {
+                    //Extract the wanted RuleSet (= ruleSet parameter) from the 
+                    //RuleDefinitions object.
+                    if (ruleDefinitions.RuleSets.Contains(ruleSet))
+                    {
+                        RuleSet rs = ruleDefinitions.RuleSets[ruleSet];
+
+                        //Next check if the rules within the RuleSet can be a applied to
+                        //the specified type of object. 
+                        RuleValidation validation = new RuleValidation(
+                            objectType, null);
+
+                        if (rs.Validate(validation))
+                        {
+                            //If the RuleSet is valid for the specified type of object, then
+                            //execute it against an instance of this class.                            
+                            RuleExecution execution =
+                                new RuleExecution(validation, instance);
+                            rs.Execute(execution);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
